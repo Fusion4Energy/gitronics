ee360bf9fea42a5da761d22b98b8b08cc0c9b64e
AlvaroCubi <55387701+AlvaroCubi@users.noreply.github.com>
2025-07-21 10:51:21 +0200

    More tests for file_discovery

--- src/gitronics/file_discovery.py
+++ src/gitronics/file_discovery.py
@@ -1,13 +1,31 @@
 from pathlib import Path
 
+ALLOWED_SUFFIXES = {".mcnp", ".transform", ".mat", ".source", ".tally"}
+
 
 def get_file_paths(project_root: Path) -> dict[str, Path]:
     if not project_root.exists() or not project_root.is_dir():
         raise FileNotFoundError(f"The directory {project_root} does not exist.")
 
-    file_paths = {}
+    # Get all file paths
+    paths_list = []
     for file in project_root.rglob("*"):
         if file.is_file():
-            file_paths[file.name] = file.resolve()
+            paths_list.append(file.resolve())
 
+    # Build dictionary while checking for duplicates and that metadata exists
+    file_paths = {}
+    for path in paths_list:
+        if path.suffix in ALLOWED_SUFFIXES:
+            file_name = path.stem
+            if file_name in file_paths:
+                raise ValueError(f"Duplicate file name found: {file_name}")
+            file_paths[file_name] = path
+            if not path.with_suffix(".metadata").exists():
+                raise FileNotFoundError(f"Metadata file not found for: {path}")
+        elif path.suffix == ".metadata":
+            continue
+        else:
+            raise ValueError(f"Unsupported file suffix for: {path}")
+    
     return file_paths
--- tests/test_file_discovery.py
+++ tests/test_file_discovery.py
@@ -4,15 +4,45 @@ import pytest
 
 from gitronics.file_discovery import get_file_paths
 
-PATH_TEST_PROJECT = Path(__file__).parent / "test_resources" / "test_project"
+PATH_TEST_RESOURCES = Path(__file__).parent / "test_resources"
 
 
 def test_get_file_paths():
-    file_paths = get_file_paths(PATH_TEST_PROJECT)
+    file_paths = get_file_paths(PATH_TEST_RESOURCES / "valid_project")
     pass
 
 
 def test_get_file_paths_directory_does_not_exist():
     non_existent_path = Path("/non/existent/path")
-    with pytest.raises(FileNotFoundError, match="The directory .* does not exist."):
+    with pytest.raises(
+        FileNotFoundError,
+        match="The directory .* does not exist.",
+    ):
         get_file_paths(non_existent_path)
+
+
+def test_get_file_paths_duplicate_file_names():
+    project_root = PATH_TEST_RESOURCES / "duplicated_filename_project"
+    with pytest.raises(
+        ValueError,
+        match="Duplicate file name found: duplicated_name",
+    ):
+        get_file_paths(project_root)
+
+
+def test_get_file_paths_missing_metadata():
+    project_root = PATH_TEST_RESOURCES / "missing_metadata_project"
+    with pytest.raises(
+        FileNotFoundError,
+        match="Metadata file not found for: .*",
+    ):
+        get_file_paths(project_root)
+
+
+def test_get_file_paths_unsupported_suffix():
+    project_root = PATH_TEST_RESOURCES / "invalid_suffix_project"
+    with pytest.raises(
+        ValueError,
+        match="Unsupported file suffix for: .*",
+    ):
+        get_file_paths(project_root)
